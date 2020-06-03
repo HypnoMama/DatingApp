3dd3e0688f60a3acb4bbd1ad308abb469ec239e8
HypnoMama <sarahhermsen@gmail.com>
2020-06-03 15:32:20 -0700

    add ExtendedUser migration.  Went thru how to drop database and recreate with seed data.  Add DatingRepository and Interface

---
+++ DatingApp.API/Data/DatingRepository.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DatingRepository : IDatingRepository
+    {
+        private readonly DataContext _context;
+        public DatingRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            //Saving in memory NOT actually adding to database at this point
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            //retrieve user by id including their photos. Returns null if not found
+            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            //return list of users from database including their photos as we will want to display their main photo
+            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+            return users;
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            //if there are changes we will save > 0 changes and this will return true
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
